7fab87238eb4989c1c2c57b523706b19b33103e5
May <pakeeranam@gmail.com>
2021-11-27 13:42:26 +1100

    edit display

--- README.md
+++ README.md
@@ -6,14 +6,36 @@
 
 This is the project 1 from my class, Software Engineering Immersive: Sydney - General Assembly.
 
-
 ### Technologies Used
+
 I am using HTML, JavaScript, and CSS.
 
-HTML         | JavaScript    | CSS 
-------------- | -------------  | ----------
-Layout  | Dom manipulation|Design games
-Content  | Interaction with users  | Animations
-Structure a web page         | Switch Player     | Presentation
+| HTML                 | JavaScript             | CSS          |
+| -------------------- | ---------------------- | ------------ |
+| Layout               | Dom manipulation       | Design games |
+| Content              | Interaction with users | Animations   |
+| Structure a web page | Switch Player          | Presentation |
 
-Developed by May 2021
+## How to play
+
+This is a two player game.
+
+**Start**
+
+The first player will pick one of the boxes to start with. The first player is "X".
+
+The second player will pick any of the boxes after the first player. The second player is "O".
+
+**Game**
+
+Players have a 3 x 3 grid to play with in. On click a token is placed onto the grid. Players alternate placing tokens until a line of three tokens in a row is formed or the grid is full of tokens.
+
+The first player who get this 3 in a row wins.
+
+**End**
+
+At the end of the game the display block shows a winner. If the player want to play again then click new game button.
+
+**Developed by** May Manmai
+
+Nov 2021
--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const playerOne = 'X';
 const playerTwo = 'O';
 let currentPlayer = playerOne;
+let yourName1 = prompt('You are the player one! What is your name?');
+let yourName2 = prompt('You are the player two! What is your name?');
 
 const jackpot = document.querySelector('.boxes');
 
@@ -41,62 +43,103 @@ function checkJackpot() {
         allBoxes[0].textContent === allBoxes[1].textContent &&
         allBoxes[1].textContent === allBoxes[2].textContent
     ) {
-        allBoxes[0].textContent = ':)';
+        allBoxes[0].textContent = '💪🤪👊';
+        allBoxes[1].textContent = '💪🤪👊';
+        allBoxes[2].textContent = '💪🤪👊';
         result(currentPlayer);
     } else if (
         allBoxes[3].textContent != '' &&
         allBoxes[3].textContent === allBoxes[4].textContent &&
         allBoxes[4].textContent === allBoxes[5].textContent
     ) {
+        allBoxes[3].textContent = '💪🤪👊';
+        allBoxes[4].textContent = '💪🤪👊';
+        allBoxes[5].textContent = '💪🤪👊';
         result(currentPlayer);
     } else if (
         allBoxes[6].textContent != '' &&
         allBoxes[6].textContent === allBoxes[7].textContent &&
         allBoxes[7].textContent === allBoxes[8].textContent
     ) {
+        allBoxes[6].textContent = '💪🤪👊';
+        allBoxes[7].textContent = '💪🤪👊';
+        allBoxes[8].textContent = '💪🤪👊';
         result(currentPlayer);
     } else if (
         allBoxes[0].textContent != '' &&
         allBoxes[0].textContent === allBoxes[3].textContent &&
         allBoxes[3].textContent === allBoxes[6].textContent
     ) {
+        allBoxes[0].textContent = '💪🤪👊';
+        allBoxes[3].textContent = '💪🤪👊';
+        allBoxes[6].textContent = '💪🤪👊';
         result(currentPlayer);
     } else if (
         allBoxes[1].textContent != '' &&
         allBoxes[1].textContent === allBoxes[4].textContent &&
         allBoxes[4].textContent === allBoxes[7].textContent
     ) {
+        allBoxes[1].textContent = '🤪👊';
+        allBoxes[4].textContent = '🤪👊';
+        allBoxes[7].textContent = '🤪👊';
         result(currentPlayer);
     } else if (
         allBoxes[2].textContent != '' &&
         allBoxes[2].textContent === allBoxes[5].textContent &&
         allBoxes[5].textContent === allBoxes[8].textContent
     ) {
+        allBoxes[2].textContent = '🤪👊';
+        allBoxes[5].textContent = '🤪👊';
+        allBoxes[8].textContent = '👊🤪';
         result(currentPlayer);
     } else if (
         allBoxes[0].textContent != '' &&
         allBoxes[0].textContent === allBoxes[4].textContent &&
         allBoxes[4].textContent === allBoxes[8].textContent
     ) {
+        allBoxes[0].textContent = '👊🤪';
+        allBoxes[4].textContent = '👊🤪';
+        allBoxes[8].textContent = '👊🤪';
         result(currentPlayer);
     } else if (
         allBoxes[2].textContent != '' &&
         allBoxes[2].textContent === allBoxes[4].textContent &&
         allBoxes[4].textContent === allBoxes[6].textContent
     ) {
+        allBoxes[2].textContent = '👊🤪';
+        allBoxes[4].textContent = '👊🤪';
+        allBoxes[6].textContent = '👊🤪';
         result(currentPlayer);
     }
 }
 
 function result(player) {
     if (player === playerOne) {
-        document.querySelector('.display').innerHTML =
-            'Player One is the winner!';
+        if (yourName2 != '') {
+            document.querySelector(
+                '.display'
+            ).innerHTML = `${yourName1} is the winner!`;
+            document.querySelector('.display').classList.add('twoWon');
+        } else {
+            document.querySelector(
+                '.display'
+            ).innerHTML = `Player One "X" is the winner!`;
+            document.querySelector('.display').classList.add('twoWon');
+        }
     } else if (player === playerTwo) {
-        document.querySelector('.display').innerHTML =
-            'Player Two is the winner!';
+        if (yourName2 != '') {
+            document.querySelector(
+                '.display'
+            ).innerHTML = `${yourName2} is the winner!`;
+            document.querySelector('.display').classList.add('twoWon');
+        } else {
+            document.querySelector(
+                '.display'
+            ).innerHTML = `Player Two "O" is the winner!`;
+            document.querySelector('.display').classList.add('twoWon');
+        }
     }
-    console.log('Hello World');
+    // console.log('Hello World');
 
     const allBoxes = document.querySelectorAll('.box');
     for (let i = 0; i < allBoxes.length; i++) {
